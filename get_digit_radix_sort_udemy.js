e73ca9ef5fe463e65f436ef70637714263de2e53
Crew Spence <csspence123@gmail.com>
2020-08-10 05:36:23 -0500

    Created function stub for toy problem.

---
+++ get_digit_radix_sort_udemy.js
@@ -0,0 +1,11 @@
+/*
+JavaScript Algorithms and Data Structures Masterclass
+getDigit
+(Helper function for Radix Sort)
+
+Write a function getDigit which takes in a number and a place and returns the digit in the number at the given place value.
+*/
+
+const getDigit = (num, place) => {
+  
+}
